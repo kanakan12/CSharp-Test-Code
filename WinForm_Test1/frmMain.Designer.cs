34f8077d0660844d10fa2d8c839bc87154a5de7c
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2020-09-23 22:50:22 +0900

    Add files via upload

---
+++ WinForm_Test1/frmMain.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WinForm_Test1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.laResult = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.laResult_1 = new System.Windows.Forms.Label();
+            this.chkExample = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(444, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(61, 12);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(446, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(419, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "이름이 무엇입니까?";
+            // 
+            // laResult
+            // 
+            this.laResult.AutoSize = true;
+            this.laResult.Location = new System.Drawing.Point(419, 228);
+            this.laResult.Name = "laResult";
+            this.laResult.Size = new System.Drawing.Size(0, 12);
+            this.laResult.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(421, 282);
+            this.txtName.MaxLength = 5;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(187, 21);
+            this.txtName.TabIndex = 5;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            this.txtName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtName_KeyDown);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(421, 310);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 6;
+            this.btnOK.Text = "확인";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // laResult_1
+            // 
+            this.laResult_1.AutoSize = true;
+            this.laResult_1.Location = new System.Drawing.Point(419, 392);
+            this.laResult_1.Name = "laResult_1";
+            this.laResult_1.Size = new System.Drawing.Size(189, 12);
+            this.laResult_1.TabIndex = 7;
+            this.laResult_1.Text = "체크 박스가 선택되지 않았습니다.";
+            // 
+            // chkExample
+            // 
+            this.chkExample.AutoSize = true;
+            this.chkExample.Location = new System.Drawing.Point(421, 420);
+            this.chkExample.Name = "chkExample";
+            this.chkExample.Size = new System.Drawing.Size(104, 16);
+            this.chkExample.TabIndex = 8;
+            this.chkExample.Text = "체크 박스 예제";
+            this.chkExample.UseVisualStyleBackColor = true;
+            this.chkExample.CheckedChanged += new System.EventHandler(this.chkExample_CheckedChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 621);
+            this.Controls.Add(this.chkExample);
+            this.Controls.Add(this.laResult_1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.laResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WinForm-EX";
+            this.Click += new System.EventHandler(this.frmMain_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label laResult;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label laResult_1;
+        private System.Windows.Forms.CheckBox chkExample;
+    }
+}
+
---
+++ WinForm_Test1/frmMain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_Test1
+{
+    public partial class frmMain : Form
+    {
+        string path = AppDomain.CurrentDomain.BaseDirectory;
+        public frmMain()
+        {
+            InitializeComponent();
+            this.Text = "코드 편집 창에서 Text 속성을 변경하였습니다.";
+
+            linkLabel1.Text = "클릭 하면 홈페이지로 이동합니다.";
+            linkLabel1.Links.Add(0, 2, "http:\\www.mircosoft.com");
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.Image = new Bitmap(path + "Test_Image.PNG");
+        }
+
+        private void frmMain_Click(object sender, EventArgs e)
+        {
+            this.Text = "클릭 했습니다.";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // 해당과 같은 방법으로 링크 추가 가능
+            System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "버튼이 눌렸습니다!!";
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            laResult.Text = "아하! 당신은" + txtName.Text + "이군요.";
+        }
+
+        private void txtName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+                laResult.Text = "아하! 당신은" + txtName.Text + "이군요.";
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            laResult.Text = "아하! 당신은" + txtName.Text + "이군요.";
+        }
+
+        private void chkExample_CheckedChanged(object sender, EventArgs e)
+        {
+            // 해당 방법은 선호하지 않음
+            // 여러개의 체크 박스를 사용할 경우 혼동여지 존재하기 때문
+            //if (chkExample.Checked)
+            //    laResult_1.Text = "체크 박스가 선택되었습니다.";
+            //else
+            //    laResult_1.Text = "체크 박스가 선택되지 않았습니다.";
+
+            CheckBox chkExam = sender as CheckBox;
+            if (chkExam.Checked)
+                laResult_1.Text = "체크 박스가 선택되었습니다.";
+            else
+                laResult_1.Text = "체크 박스가 선택되지 않았습니다.";
+        }
+    }
+}
